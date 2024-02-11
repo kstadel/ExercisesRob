74fd33edee097f4164ff930a7124e9a15a0b7e51
Katja Stadel <stadel@newsaktuell.de>
2024-02-11 18:23:48 +0100

    add 4th exercise

---
+++ easy/exercise_4.js
@@ -0,0 +1,13 @@
+const arr = [2,30,4,0,10,15]
+
+// use a for loop (or while loop) to sort this array's numbers
+// in ascending order
+
+// look here for inspiration to solve this
+// https://stackoverflow.com/questions/38331143/sort-an-array-containing-numbers-using-a-for-loop
+
+let sortedArray = []
+
+
+
+// expected sortedArray = [0, 2, 4, 10, 15, 30]
---
+++ solutions/solution_easy_4.js
@@ -0,0 +1,21 @@
+const arr = [2,30,4,0,10,15]
+let sortedArray = []
+
+
+
+function sortedFunction(arr) {
+  let sortedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let n = 0;
+      if (arr[i] > arr[j]) {
+        n = arr[i];
+        arr[i] = arr[j];
+        arr[j] = n;
+      }
+    }
+    sortedArr.push(arr[i]);
+  }
+  return sortedArr;
+}
+console.log(sortedFunction(arr));
