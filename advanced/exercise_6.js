d520d2286272237e62c8b775d6637a66927e8c01
Katja Stadel <stadel@newsaktuell.de>
2024-02-11 19:03:18 +0100

    add another exercise

---
+++ advanced/exercise_6.js
@@ -0,0 +1,26 @@
+const people = [[{name: "Anna",
+gender: "Female"}, {name: "Tom",
+gender: "Male"},{name: "Elaine",
+gender: "Female"}],[{name: "Maria",
+gender: "Female"},{name: "Bill",
+gender: "Male"}],[{name: "Lucy",
+gender: "Female"}],[{name: "Hannah",
+gender: "Female"},{name: "Grace",
+gender: "Female"},{name: "Berry",
+gender: "Male"}]]
+
+// 1)
+// sort the people according to their gender and store them in two arrays
+// females: [{name: "Anna",gender: "Female"}...]
+// males: [{name: "Bill",gender: "Male"}...]
+
+// use nested for loop
+
+
+// 2)
+// copy paste and rewrite your nested for loop so that only 
+// their names are in the arrays so that the outcome
+// of your nested for-loop would be sth. like
+
+// females: ["Anna", "Elaine"...]
+// males: ["Tom", "Bill"...]
