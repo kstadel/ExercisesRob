56c5d0f6ab39cd264b74e203d76038e084edd2a7
Katja Stadel <stadel@newsaktuell.de>
2024-02-11 18:02:54 +0100

    add exercise 2

---
+++ easy/exercise_2.js
@@ -0,0 +1,10 @@
+const Arr = ["good", "everyone", "morning"]
+
+const Sentence = Arr[0]+' '+Arr[2]+' '+Arr[1]
+console.log(Sentence)
+
+// use your knowledge about how to access values of arrays at certain positions
+// to bring these array in correct order and concatenate them to a sentence
+// do not forget to put empty spaces into it as well
+
+// expected result: "good morning everyone"
